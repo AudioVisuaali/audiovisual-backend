dac451b0c7a996682f40d9e1b9ac3618439f2b59
Audiovisuaali <aleksi.vuorio@yahoo.com>
2019-11-24 02:40:00 +0200

    add environment variables

--- api/server.js
+++ api/server.js
@@ -1,6 +1,17 @@
+const dotenv = require('dotenv');
 const app = require('./app');
 
-const port = process.env.PORT || 3001;
+// Set environment
+
+const isProduction = process.env.NODE_ENV === 'production';
+const environment = isProduction ? 'production' : 'default';
+
+const { error } = dotenv.config({ path: `environment.${environment}.env` });
+if (error) throw error;
+
+// Config app
+
+const port = process.env.PORT || 3000;
 
 const application = new app(port);
 application.start();
