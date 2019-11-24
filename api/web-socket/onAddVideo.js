2e5b66f0a5d24481219436962df9ccbf720d3c72
Audiovisuaali <aleksi.vuorio@yahoo.com>
2019-11-24 02:45:39 +0200

    refractor message sent on addVideo

--- api/web-socket/onAddVideo.js
+++ api/web-socket/onAddVideo.js
@@ -12,39 +12,34 @@ function onAddvideo(socket, videoInformation) {
   createVideo(videoInformation, user).then(video => {
     if (!video) return;
 
-    let instantlyPlayingMessage;
+    let messageResponse;
     const room = socket.getVisualsRoom(roomUnique);
 
     if (room.videos.playing) {
       room.videos.playlist.push(video);
+
+      messageResponse = messageUtil.createUserMessage(
+        video,
+        user,
+        messageUtil.MESSAGE_VIDEO_ADD
+      );
     } else {
       room.videos.playing = video;
       room.timelineAction.updatedAt = new Date().toString();
       room.timelineAction.seeked = 0;
       room.timelineAction.action = room.playing ? 'play' : 'true';
-      instantlyPlayingMessage = messageUtil.createUserMessage(
+
+      messageResponse = messageUtil.createUserMessage(
         room.videos.playing,
         user,
         messageUtil.MESSAGE_VIDEO_NEXT
       );
     }
 
-    const messageResponse = messageUtil.createUserMessage(
-      video,
-      user,
-      messageUtil.MESSAGE_VIDEO_ADD
-    );
-
     room.messages.push(messageResponse);
-    if (instantlyPlayingMessage) {
-      room.messages.push(instantlyPlayingMessage);
-    }
     socket.updateVisualsRoom(roomUnique, room);
 
     socket.sendToRoom(roomUnique, WS_TYPES.MESSAGE, messageResponse);
-    if (instantlyPlayingMessage) {
-      socket.sendToRoom(roomUnique, WS_TYPES.MESSAGE, instantlyPlayingMessage);
-    }
     socket.sendToRoom(roomUnique, WS_TYPES.ADD_VIDEO, room.videos);
   });
 }
